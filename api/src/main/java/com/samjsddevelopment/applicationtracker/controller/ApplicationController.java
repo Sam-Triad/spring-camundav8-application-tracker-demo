87b2bf3744190b583d323f51d7c345aa726de04b
Sam-Douglas <sdouglas.contact@gmail.com>
2025-09-24 17:05:26 +0100

    Refactored Controllers

--- api/src/main/java/com/samjsddevelopment/applicationtracker/controller/ApplicationController.java
+++ api/src/main/java/com/samjsddevelopment/applicationtracker/controller/ApplicationController.java
@@ -2,7 +2,6 @@ package com.samjsddevelopment.applicationtracker.controller;
 
 import java.util.UUID;
 
-import com.samjsddevelopment.applicationtracker.dto.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,42 +11,35 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.samjsddevelopment.applicationtracker.service.ApplicantService;
+import com.samjsddevelopment.applicationtracker.dto.ApplicationDto;
+import com.samjsddevelopment.applicationtracker.dto.CreateApplicationRequest;
+import com.samjsddevelopment.applicationtracker.dto.UpdateApplicationRequest;
 import com.samjsddevelopment.applicationtracker.service.ApplicationService;
-import com.samjsddevelopment.applicationtracker.service.ReviewerService;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/applications")
 public class ApplicationController {
-
-    private final ApplicantService applicantService;
-    private final ReviewerService reviewerService;
+    
     private final ApplicationService applicationService;
 
-    @PostMapping("/applicants")
-    public ResponseEntity<ApplicantDto> createApplicant(@RequestBody CreateApplicantRequest request) {
-        return new ResponseEntity<>(applicantService.createApplicant(request), HttpStatus.CREATED);
-    }
-
-    @PostMapping("/reviewers")
-    public ResponseEntity<ReviewerDto> createReviewer(@RequestBody CreateReviewerRequest request) {
-        return new ResponseEntity<>(reviewerService.createReviewer(request), HttpStatus.CREATED);
-    }
-
-    @PostMapping("/applications")
+    @PostMapping
+    @Operation(summary = "Create a new application")
     public ResponseEntity<ApplicationDto> createApplication(@RequestBody CreateApplicationRequest request) {
         return new ResponseEntity<>(applicationService.createApplication(request), HttpStatus.CREATED);
     }
 
-    @PutMapping("/applications/{id}")
+    @PutMapping("/{id}")
+    @Operation(summary = "Update an application")
     public ResponseEntity<ApplicationDto> updateApplication(@PathVariable UUID id, @RequestBody UpdateApplicationRequest request) {
         return ResponseEntity.ok(applicationService.updateApplication(id, request));
     }
 
-    @PostMapping("/applications/{id}/submit")
+    @PostMapping("/{id}/submit")
+    @Operation(summary = "Submit an application for review")
     public ResponseEntity<Void> submitApplication(@PathVariable UUID id) {
         applicationService.submitApplication(id);
         return ResponseEntity.accepted().build();
