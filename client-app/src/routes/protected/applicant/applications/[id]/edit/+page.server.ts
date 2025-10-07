5ac0d0b976dd75c1a3e13c8c014197b27e77366d
Sam-Douglas <sdouglas.contact@gmail.com>
2025-10-07 13:54:48 +0100

    Commited missing load function

--- client-app/src/routes/protected/applicant/applications/[id]/edit/+page.server.ts
+++ client-app/src/routes/protected/applicant/applications/[id]/edit/+page.server.ts
@@ -3,6 +3,24 @@ import type { Actions, PageServerLoad } from './$types';
 import { getApplication, updateApplication } from '$lib/api/api';
 import type { Application } from '$lib/api/types';
 
+export const load: PageServerLoad = async ({ params, fetch, locals }) => {
+	const session = await locals.auth();
+
+	const apiResponse: Application = await getApplication(
+		params.id,
+		{ fetch, accessToken: session?.accessToken }
+	);
+
+	if (apiResponse.applicationStatus !== 'WAITING_FOR_SUBMISSION') {
+		throw redirect(303, `/applications/${params.id}`);
+	}
+
+	return {
+		title: 'Edit Application',
+		apiResponse
+	};
+};
+
 export const actions: Actions = {
 	update: async ({ request, params, fetch, locals }) => {
 		const session = await locals.auth();
